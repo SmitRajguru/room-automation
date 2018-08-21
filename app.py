11223f255c4622acd72296b1f62cc4ace5d2b95a
Smit Makarand Rajguru <rajgurusmit1997@gmail.com>
2018-08-21 22:28:13 +0530

    Adding port update mqtt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , request
+from Adafruit_IO import MQTTClient
 import psycopg2
 
 hostname = 'ec2-54-163-246-5.compute-1.amazonaws.com'
@@ -6,11 +7,53 @@ username = 'ccwbkzomgaturz'
 password = '86103e1ac176041a672dd0ac069edeee94780fa94a49162f80b18a0cef599af8'
 database = 'd9uvpss3s3f0k'
 
+ADAFRUIT_IO_KEY = 'a8ce257319334cd29630749102997271'
+ADAFRUIT_IO_USERNAME = 'rajgurusmit'
+
+FEED_ID_2 = 'update'
+
 app = Flask(__name__)
 
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 
 cur = myConnection.cursor()
+
+# Define callback functions which will be called when certain events happen.
+def connected(client):
+	# Connected function will be called when the client is connected to Adafruit IO.
+	# This is a good place to subscribe to feed changes.  The client parameter
+	# passed to this function is the Adafruit IO MQTT client so you can make
+	# calls against it easily.
+	print('Connected to Adafruit IO!  Listening for {0} changes...'.format(FEED_ID_2))
+	# Subscribe to changes on a feed named DemoFeed.
+	client.subscribe(FEED_ID_2)
+
+def disconnected(client):
+	# Disconnected function will be called when the client disconnects.
+	print('Disconnected from Adafruit IO!')
+	sys.exit(1)
+
+def message(client, feed_id, payload):
+	# Message function will be called when a subscribed feed has a new value.
+	# The feed_id parameter identifies the feed, and the payload parameter has
+	# the new value.
+	print('Feed {0} received new value: {1}'.format(feed_id, payload))
+
+# Create an MQTT client instance.
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+# Setup the callback functions defined above.
+client.on_connect    = connected
+client.on_disconnect = disconnected
+client.on_message    = message
+
+# Connect to the Adafruit IO server.
+client.connect()
+
+# Start a message loop that blocks forever waiting for MQTT messages to be
+# received.  Note there are other options for running the event loop like doing
+# so in a background thread--see the mqtt_client.py example to learn more.
+client.loop_background()
 
 @app.route('/')
 def rebound():
@@ -46,11 +89,13 @@ def setPort():
 		print("Adding new feed of " + request.json['feed'] + " to the database")
 		cur.execute("insert into feeds(feed) values('" + request.json['feed'] + "');")
 		myConnection.commit()
+		client.publish(FEED_ID_2, "True", ADAFRUIT_IO_USERNAME)
 		return "{New data}"
 	else:
 		print("Updating feed of " + request.json['feed'] + " to the port of " + request.json['port'])
 		cur.execute("update feeds set port = %s where feed = %s;",(request.json['port'],request.json['feed']))
 		myConnection.commit()
+		client.publish(FEED_ID_2, "True", ADAFRUIT_IO_USERNAME)
 		return "{updated data}"
 
 @app.route('/getValue', methods=['POST'])
